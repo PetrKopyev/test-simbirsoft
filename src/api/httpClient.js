0aeee3f7dc48fe2915252dd727f9942f37acc923
PetrKopyev <petrkopev@gmail.com>
2021-06-01 19:51:15 +0400

    add Headers for CORS

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -4,8 +4,13 @@ import {apiURL, apiToken} from '@/constants/api.constants';
 const httpClient = axios.create({
     baseURL: apiURL,
     headers: {
+        // "Access-Control-Allow-Methods": "GET",
+        // "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Headers": "x-auth-token, x-response-control",
+        // "Content-Length": 0,
+        // "Content-Type": "text/plain",
         "X-Auth-Token": apiToken,
-    }
+    },
 })
 
 export default httpClient;
